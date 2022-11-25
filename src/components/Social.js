bce988b43e173b890a5429e7209410aab18cb85e
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2022-11-25 23:09:25 +0600

    ui changed

--- src/components/Social.js
+++ src/components/Social.js
@@ -2,22 +2,25 @@ import React from 'react'
 import { SiFacebook, SiGithub, SiGmail, SiInstagram, SiLinkedin } from "react-icons/si";
 
 const Social = () => {
+     const style ={
+          color: "#a4adba"
+     }
      return (
           <div className='flex justify-center items-center gap-x-3 mt-8'>
-               <a href='https://www.facebook.com/MarzanB1nRahman/' target="_blank">
-                    <SiFacebook className='w-4 md:w-8 h-4 md:h-8 cursor-pointer hover:scale-125 duration-400 transition-all ease-linear' />
+               <a href='https://www.facebook.com/MarzanB1nRahman/' target="_blank" rel="noreferrer">
+                    <SiFacebook className='w-6 md:w-8 h-6 md:h-8 cursor-pointer hover:scale-125 duration-400 transition-all ease-linear' style={style}/>
                </a>
-               <a href='https://www.instagram.com/MarzanBinRahman/' target="_blank">
-                    <SiInstagram className='w-4 md:w-8 h-4 md:h-8 cursor-pointer  hover:scale-125 duration-400 transition-all ease-linear' />
+               <a href='https://www.instagram.com/MarzanBinRahman/' target="_blank" rel="noreferrer">
+                    <SiInstagram className='w-6 md:w-8 h-6 md:h-8 cursor-pointer  hover:scale-125 duration-600 transition-all ease-linear' style={style}/>
                </a>
-               <a href='https://www.linkedin.com/in/marzan-bin-rahman-9400a0223/' target="_blank">
-                    <SiLinkedin className='w-4 md:w-8 h-4 md:h-8 cursor-pointer hover:scale-125 duration-400 transition-all ease-linear' />
+               <a href='https://www.linkedin.com/in/marzan-bin-rahman-9400a0223/' target="_blank" rel="noreferrer">
+                    <SiLinkedin className='w-6 md:w-8 h-6 md:h-8 cursor-pointer hover:scale-125 duration-600 transition-all ease-linear' style={style}/>
                </a>
-               <a href='https://github.com/Marzan-Bin-Rahman' target="_blank">
-                    <SiGithub className='w-4 md:w-8 h-4 md:h-8 cursor-pointer  hover:scale-125 duration-400 transition-all ease-linear' />
+               <a href='https://github.com/Marzan-Bin-Rahman' target="_blank" rel="noreferrer">
+                    <SiGithub className='w-6 md:w-8 h-6 md:h-8 cursor-pointer  hover:scale-125 duration-600 transition-all ease-linear' style={style}/>
                </a>
-               <a href='mailto:mailhereformarzan@gmail.com' target="_blank">
-                    <SiGmail className='w-4 md:w-8 h-4 md:h-8 cursor-pointer  hover:scale-125 duration-400 transition-all ease-linear' />
+               <a href='mailto:mailhereformarzan@gmail.com' target="_blank" rel="noreferrer">
+                    <SiGmail className='w-6 md:w-8 h-6 md:h-8 cursor-pointer  hover:scale-125 duration-600 transition-all ease-linear' style={style}/>
                </a>
           </div>
      )
