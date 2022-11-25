928925a980f8378af3aefae2b36d7ee72a015cfb
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2022-11-25 22:21:19 +0600

    ui update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,9 +9,9 @@ const HeroSection = () => {
   return (
      <div className="hero min-h-screen bg-slate-700" style={style}>
      <div className="hero-content text-center">
-       <div className="">
-          <img src={me} className="w-24 h-24 md:w-36 md:h-36 rounded-full block mx-auto my-10 ring-4 ring-amber-500"/>
-          <h1 className='text-3xl md:text-4xl font-black'>Assalamualaikum</h1>
+       <div>
+          <img src={me} className="w-24 h-24 md:w-36 md:h-36 rounded-full block mx-auto mb-10 ring-4 ring-amber-500"/>
+          <h1 className='text-3xl md:text-4xl lg:text-7xl font-black'>Assalamualaikum</h1>
          <TypedAnimation />
        </div>
      </div>
