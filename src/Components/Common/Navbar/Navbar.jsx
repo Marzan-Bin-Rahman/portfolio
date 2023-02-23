9e92a3ca521296116b433062bbe0312824716672
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-02-24 00:24:17 +0600

    navabar updated

--- src/Components/Common/Navbar/Navbar.jsx
+++ src/Components/Common/Navbar/Navbar.jsx
@@ -3,18 +3,15 @@ import React from 'react'
 import { useState } from "react";
 
 
-
-
 const Navbar = () => {
+
      const [navbar, setNavbar] = useState(false);
+
   return (
      <nav className="w-full bg-nav backdrop-blur-lg font-black fixed top-0 z-20">
      <div className="justify-center items-center px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
          <div>
              <div className="flex items-center justify-between py-3 md:py-5 md:block ">
-                 {/* <a href='#'>
-                     <h2 className="text-2xl lg:text-4xl font-bold font-amaranth text-lightBlue">Mohammed Adib</h2>
-                     </a> */}
                  <div className="md:hidden">
                      <button
                          className="p-2 text-gray-700 rounded-md outline-none focus:border-lightYellow focus:border"
