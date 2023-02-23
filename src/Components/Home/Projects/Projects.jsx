f51c5aed144919b954b6b300b47f1634d063aa70
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-02-24 01:01:30 +0600

    projects updated

--- src/Components/Home/Projects/Projects.jsx
+++ src/Components/Home/Projects/Projects.jsx
@@ -10,22 +10,22 @@ const Blogs = () => {
                id: 1,
                name: 'JB Mavericks',
                link : 'https://jb-mavericks.github.io/',
-               description: "As a backend Engineer on the Mentoring app project,I was responsible for designing, developing, and maintaining the server-side of the application.I contributed to the project including developing the API for user authentication,  designing the database that stored user data, mentor profiles, and other critical information. This involved optimizing the database to ensure high performance and scalability to handle large volumes of data. I also integrated several third-party APIs into the application to enhance its functionality and provide a seamless user experience. These APIs were consumed by the app to enable features such as OTP and scheduling sessions with mentors. As the creator of the app's backend, I worked closely with the front-end developers to ensure that the application worked seamlessly across all devices and platforms.I also played a critical role in ensuring the application's security and compliance with industry standards.",
-               role: 'Lead Backend Engineer'
+               description: "I both designed and developed the official website of JB Mavericks and I'm thrilled to say that it turned out awesome! With a modern design and easy-to-navigate features, visitors can quickly learn about my skills, experience, and achievements. It was a fun project, and I'm proud of it!",
+               role: 'Lead Fronted Developer and Designer'
           },
           {
                id: 2,
-               name: 'Shoppi Mart E Commerce',
-               link : 'https://github.com/ADIB-the-NOOB/Shoppi-Mart-E-Commerce',
-               description: 'As a backend developer in an E-commerce project, your primary responsibility was to design and develop the server-side of the application that powered the online store. I was responsible for developing the APIs that enabled the front-end interface to communicate with the server, managing the database that stored product and customer data, and ensuring the security and scalability of the application.',
-               role: 'Lead Backend Engineer'
+               name: 'Mavericks WriteAI',
+               link : 'https://github.com/JB-Mavericks/Mavericks-WriteAI',
+               description: "Check out Mavericks-WriteAI on GitHub! I'm thrilled to have been one of two contributors for this project. Together, we developed an innovative tool for generating human-like text using AI technology. This project was a challenging but rewarding experience, and I'm excited to see where it goes in the future.",
+               role: 'Frontend Developer'
           },
           {
                id: 3,
-               name: 'Reddits Backend',
-               link : 'https://github.com/ADIB-the-NOOB/Reddits-Backend',
-               description: "Developed a mini social media's backend",
-               role: 'Lead Backend Engineer'
+               name: 'Bill Koto',
+               link : 'https://github.com/Marzan-Bin-Rahman/bill-koto',
+               description: "Bill Koto is a small web application which can calculate the electricity bill of Bangladesh.",
+               role: 'Frontend Developer'
           }
      ]
 
