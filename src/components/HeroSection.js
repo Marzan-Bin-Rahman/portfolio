4125c3320e4898cb1c8f3a1e8705332b92f383ba
Marzan-Bin-Rahman <mailhereformarzan@gmail.com>
2023-02-23 23:54:01 +0600

    random update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -16,7 +16,7 @@ const HeroSection = () => {
 
           <img src={me} className="w-24 h-24 md:w-36 md:h-36 rounded-full block mx-auto mb-10 ring-4 ring-amber-500" alt="This handsome man is me"/>
 
-          <h1 className='text-3xl md:text-4xl lg:text-7xl font-black text-slate-300'>Assalamu'alaikum</h1>
+          <h1 className='text-3xl md:text-4xl lg:text-7xl font-black text-slate-300'>Assalamu'alaikum wa rahmatulloh</h1>
 
          <TypedAnimation />
 
